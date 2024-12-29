2ae2c2dc03d668a6a080f814fd9790054789d864
Walter Pereira Da Silva <waltersilva5@outlook.com>
2024-12-29 15:57:45 -0300

    feature initial migration

---
+++ prisma/migrations/20241229185717_initial/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `TB_USER` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(50) NOT NULL,
+    `lastName` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `sessionToken` VARCHAR(255) NULL,
+    `role` ENUM('USER', 'MANAGER', 'INSTRUCTOR', 'ADMIN') NOT NULL DEFAULT 'USER',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `TB_USER_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
